1441ad832893344f2625cb2163ff771a8670dc32
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-31 14:02:21 +0100

    style: Maquetación completa del home
    Se completó la maquetación del home del sitio web, incluyendo:
    - Estructura HTML finalizada con secciones y elementos necesarios.
    - Estilos CSS aplicados para lograr el diseño deseado.
    - Se agregaron imágenes y otros elementos visuales según las especificaciones del diseño.
    - Se implementó la responsividad para garantizar una buena visualización en diferentes dispositivos.
    
    Cierre #1

--- hotel/src/components/Blog.tsx
+++ hotel/src/components/Blog.tsx
@@ -1,3 +1,5 @@
+import { MdAdd } from "react-icons/md";
+
 interface ContentBlog {
    img: string,
    alt: string,
@@ -8,12 +10,13 @@ interface ContentBlog {
 
 const Blog = ({ img, alt, title, content }: ContentBlog) => {
    return (
-      <section>
-         <figure>
-            <img src={img} alt={alt} />
-            <figcaption>{title}</figcaption>
+      <section className="p-2 relative">
+         <figure className="h-32 overflow-hidden">
+            <img className="object-cover" src={img} alt={alt} />
          </figure>
-         <p>{content}</p>
+         <h3 className="text-sm text-background-second mt-3">{title}</h3>
+         <p className="text-xs text-color-text-primary">{content}</p>
+         <MdAdd className="text-xl text-background-second ml-auto absolute bottom-1 right-3" />
       </section>
    )
 }
--- hotel/src/page/Home.tsx
+++ hotel/src/page/Home.tsx
@@ -5,16 +5,16 @@ import ImageTwo from '../assets/image2.jpg'
 
 const Home = () => {
    return (
-      <div className="bg-main-background w-full h-full bg-cover bg-no-repeat bg-left flex flex-row">
+      <div className="bg-main-background w-full h-full bg-cover bg-no-repeat bg-left flex flex-row ">
 
 
-         <h1 className="mt-[400px] w-full h-[150px] grid items-center text-4xl text-color-text-primary pl-2 bg-backgroun-title border-l-[20px] border-background-second">Disruta de los placeres del Hotel</h1>
+         <h1 className="absolute md:relative z-10 mt-[400px] w-full h-[180px] md:h-[198px] grid items-center text-4xl md:text-4xl text-color-text-primary pl-2 bg-backgroun-title border-l-[20px] border-background-second leading-[3rem] lg:text-6xl ">Disfruta de los grandes placeres del Hotel</h1>
 
-         <div className="w-[450px] ml-auto">
+         <div className="w-[300px] md:w-[450px] ml-auto">
 
-            <button className="bg-background-second w-full border-l-[15px] border-[#946002] h-[50px]">Haz tu reserva desde aqui</button>
+            <button className="bg-background-second w-full border-l-[15px] border-[#946002] h-[70px] my-10 text-xl">Haz tu reserva desde aqui</button>
 
-            <section className="hidden md:block" >
+            <section className="hidden md:block bg-[#262626]">
 
                <Blog
                   img={ImageOne}
