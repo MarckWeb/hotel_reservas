d3eb947ea98effea62c1e89951758db8621923c7
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-02-12 19:57:09 +0100

    add a method  post  for saving many images

--- hotel_front/src/page/Reservas.tsx
+++ hotel_front/src/page/Reservas.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react'
+
 const Reservas = () => {
+   const [hotel, setHotel] = useState()
+
+   const callApi = async () => {
+      const res = await fetch('http://localhost:3000/room')
+      const data = await res.json()
+      console.log(data)
+      setHotel(data)
+   }
+
+   useEffect(() => {
+      callApi()
+   }, [])
    return (
       <div className="w-full h-screen bg-reserva-background text-white">
-         <img className="w-52 h-[450px] " src="img/dorm.png" alt="" />
+         {/* {hotel &&
+            hotel.map((room) => {
+               return (
+                  <section key={room._id}>
+                     <img
+                        className="w-[250px] h-[300px] object-cover"
+                        src={room.image}
+                        alt=""
+                     />
+                     <h3>{room.title}</h3>
+                     <p>{room.description}</p>
+                  </section>
+               )
+            })} */}
       </div>
    )
 }
