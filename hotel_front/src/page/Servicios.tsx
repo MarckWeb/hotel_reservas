076a74d36ef42fd46ad6be5fd225f787c6db20d1
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-02-25 18:39:21 +0100

    add file reservation for display the crud

--- hotel_front/src/page/Servicios.tsx
+++ hotel_front/src/page/Servicios.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { initializeServices } from '../reducer/service/service'
 import { AppDispatch, RootState } from '../app/store'
