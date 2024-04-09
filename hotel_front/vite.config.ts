e5bc4c3c5dfcc7b017051b310d15d74dd2ee5990
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-04-09 10:07:53 +0200

    config vite

--- hotel_front/vite.config.ts
+++ hotel_front/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    // puedes añadir otras opciones de construcción aquí si es necesario
+  },
 })
